54d550e7d96fe3a2eb1e1645061823b5a4a91024
Lennart R <lr@accessowl.io>
2022-08-08 12:45:59 +0200

    Add working example without flop usage

--- test/mcve_test.exs
+++ test/mcve_test.exs
@@ -2,8 +2,17 @@ defmodule MCVETest do
   use ExUnit.Case
   use MCVE.RepoCase
 
+  test "not using filtering returns" do
+    create_application()
+
+    app = MCVE.Repo.one(MCVE.App) |>  Repo.preload([tags: :tag])
+    assert app.tags |> Enum.map(& &1.tag.title) |> Enum.sort() == ["hello", "how_r_u"]
+  end
+
+
   test "total count is total of app entities" do
     create_application()
+
     {:ok, {apps, meta}} = MCVE.list_apps_by_flop(%{})
     assert length(apps) == 1
     assert meta.total_count == 1
